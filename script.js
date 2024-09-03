1a7b5f44379a646da5782c99e4e97e9a35b8c998
Anderson Dutra <andersonportugaall@gmail.com>
2024-09-03 16:01:34 -0300

    segundo comit

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-const options = document.querySelectorAll(".option-img")
-
-function clic(event) {
-    console.log(event.currentTarget)
-}
-
-
-options.forEach( img => {
-   img.addEventListener("click", clic)
-})
+console.log('Anderson voce e lindão')
